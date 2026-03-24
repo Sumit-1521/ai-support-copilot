febb5b3285b9f58fb58bab84f4fcf8edc4300927
Sumit-1521 <sumitkumarsheoran89@gmail.com>
2026-03-24 22:57:37 +0530

    Fix dependency conflicts for Render deploy

--- app/main.py
+++ app/main.py
@@ -9,8 +9,4 @@ def home():
     return {"message": "AI Customer Support Copilot is running 🚀"}
 
 
-<<<<<<< HEAD
 app.include_router(router)
-=======
-app.include_router(router)
->>>>>>> e0209a8 (Removed large files and fixed repo size)
