cff127823384d816850f8bd66bf1d5710a60092e
Sumit Kumar Sheoran <sumitkumarsheoran89@gmail.com>
2026-03-24 21:05:36 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -2,7 +2,9 @@ from fastapi import FastAPI
 from app.routes.query import router
 from app.services.rag_pipeline import initialize_pipeline
 from app.services.retriever import load_faiss
-
+git add .
+git commit -m "Fix Render port binding"
+git push
 app = FastAPI(title="AI Customer Support Copilot")
 
 
